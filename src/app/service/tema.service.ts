64e30cd6cb0e5e91e3047226936b9b389d94f5ff
Fernando Caruso <luisfernandocaruso95@gmail.com>
2021-08-22 22:10:31 -0300

    Alterações no layout e imagem do rodapé atualizada

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(private http: HttpClient) { }
+
+  token={
+  headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]> {
+    return this.http.get<Tema[]>('https://olamundobackend.herokuapp.com/tema')
+  }
+
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://olamundobackend.herokuapp.com/tema/${id}`)
+  }
+
+  getByTemaPostagem(temapostagem: string): Observable<Tema> {
+    return this.http.get<Tema>(`https://olamundobackend.herokuapp.com/tema/temapostagem/${temapostagem}`)
+  }
+
+  getByAmbientacao(ambientacao: string): Observable<Tema> {
+    return this.http.get<Tema>(`https://olamundobackend.herokuapp.com/tema/ambientacao/${ambientacao}`)
+  }
+
+  getByPalavraChave(palavrachave: string): Observable<Tema[]> {
+    return this.http.get<Tema[]>(`https://olamundobackend.herokuapp.com/tema/palavrachave/${palavrachave}`)
+  }
+
+  postTema(tema: Tema): Observable<Tema> {
+    return this.http.post<Tema>('https://olamundobackend.herokuapp.com/tema', tema)
+  }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>('https://olamundobackend.herokuapp.com/tema', tema)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://olamundobackend.herokuapp.com/tema/${id}`)
+  }
+}
